2ded3cdb1ca8c866ed242055774ec06261e0a45c
Robert Wagner <rob.justin.wagner@gmail.com>
2016-05-19 01:17:29 -0700

    parsing done, algorithm prep

--- HackerRank/algorithms/Search_SherlockAndArray.java
+++ HackerRank/algorithms/Search_SherlockAndArray.java
@@ -1,14 +1,28 @@
 package algorithms;
 
+import java.util.HashMap;
+
 /*
-Watson gives Sherlock an array A of length N. Then he asks him to determine if there exists an element in the array such that the sum of the elements on its left is equal to the sum of the elements on its right. If there are no elements to the left/right, then the sum is considered to be zero. 
+Sherlock and Array
+==================
+
+PROBLEM:
+Watson gives Sherlock an array A of length N. Then he asks him to determine if 
+there exists an element in the array such that the sum of the elements on its 
+left is equal to the sum of the elements on its right. If there are no elements 
+to the left/right, then the sum is considered to be zero. 
+
 Formally, find an i, such that, A1 + A2 + ... + Ai-1 = Ai+1 + Ai+2 + An.
 
 Input Format 
-The first line contains T, the number of test cases. For each test case, the first line contains N, the number of elements in the array A. The second line for each test case contains N space-separated integers, denoting the array A.
+The first line contains T, the number of test cases. For each test case, the first 
+line contains N, the number of elements in the array A. The second line for each test 
+case contains N space-separated integers, denoting the array A.
 
 Output Format 
-For each test case print YES if there exists an element in the array, such that the sum of the elements on its left is equal to the sum of the elements on its right; otherwise print NO.
+For each test case print YES if there exists an element in the array, such that 
+the sum of the elements on its left is equal to the sum of the elements on its right; 
+otherwise print NO.
 
 Constraints 
 1 <= T <= 10
@@ -35,6 +49,46 @@ https://www.hackerrank.com/challenges/sherlock-and-array
 @author	rob.wagner
 */
 
+import java.util.Scanner;
+
 public class Search_SherlockAndArray {
 
+	private static Scanner stdIn;
+	private static HashMap<Integer, Integer[]> testMap;
+	
+	private final static String BALANCED = "YES";
+	private final static String NOT_BALANCED = "NO";
+	
+	public static void main(String[] args){
+		
+		stdIn = new Scanner(System.in);
+		testMap = new HashMap<Integer, Integer[]>();
+		
+		//handle parsing
+		int T = stdIn.nextInt();	//number of tests
+		for(int testCaseNumber = 1; testCaseNumber <= T; testCaseNumber++){
+			parseArray(testCaseNumber);
+			boolean balanced = isArrayBalanced(testCaseNumber);
+			String message = balanced ? BALANCED : NOT_BALANCED;
+			System.out.println(message);
+		}
+	}
+	
+	//NOTE: keeping modular for future unit testing
+	
+	private static void parseArray(int testCaseNumber){
+		//parse TC into int[] / HashMap
+		Integer testCaseLen = stdIn.nextInt();
+		Integer[] A = new Integer[testCaseLen];
+		for (int i = 0; i < testCaseLen; i++){
+			A[i] = stdIn.nextInt();
+		}
+		testMap.put(Integer.valueOf(testCaseNumber), A);
+	}
+	
+	private static boolean isArrayBalanced(int testCaseNumber){
+		
+		return true;
+	}
+	
 }
