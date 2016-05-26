6ea1ea2df8aff9db4a08c9192e78f922dd46d827
Robert Wagner <rob.justin.wagner@gmail.com>
2016-05-25 23:58:54 -0700

    .

--- HackerRank/algorithms/Search_MaximizeSum.java
+++ HackerRank/algorithms/Search_MaximizeSum.java
@@ -13,36 +13,36 @@ where there are N x (N+1)/2 possible subarrays.
 For a given array A[] of size N, subarray is a contiguous segment 
 from i to j where o <= i <= j <= N
 
-Input Format 
+Input Format: 
 First line contains T , number of test cases to follow. Each test 
 case consists of exactly 2 lines. First line of each test case contain 
 2 space separated integers N and M, size of the array and modulo value M. 
 Second line contains N space separated integers representing the elements of the array.
 
-Output Format 
+Output Format:
 For every test case output the maximum value asked above in a newline.
 
-Constraints 
+Constraints: 
 2 <= N <= 10^5
 1 <= M <= 10^14
 1 <= elements of the array <= 10^18
 2 <= Sum of N over all test cases <= 500000 
 
-Sample Input
+Sample Input:
 1
 5 7
 3 3 9 9 5
 
-Sample Output
+Sample Output:
 6
 
-Explanation
-Possible subarrays are 
+Explanation:
+Possible subarrays are: 
 {3},{3},{9},{9},{5}
 {3,3},{3,9},{9,9},{9,5}
 {3,3,9},{3,9,9},{9,9,5}
 {3,3,9,9},{3,3,9,9,5},{3,9,9,5}
-their sums modulo 7 are
+their sums modulo 7 are:
 3,3,2,2,5,6,5,4,0,1,0,2,3,1,5
 respectively. 
 Hence maximum possible sum taking Modulo 7 is 6 , and we can get 6 by adding 
